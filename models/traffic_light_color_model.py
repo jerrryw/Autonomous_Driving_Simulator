56cbca4295199437d73f4efade24e1833589cbb0
Jerry Wang <34264605+jerrryw@users.noreply.github.com>
2025-06-18 21:15:44 -0400

    formatting + update path

--- models/traffic_light_color_model.py
+++ models/traffic_light_color_model.py
@@ -11,9 +11,9 @@ class TrafficLightCNN(nn.Module):
         super(TrafficLightCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 16 * 16, 64)
-        self.fc2 = nn.Linear(64, 4)  # 4 classes
+        self.pool  = nn.MaxPool2d(2, 2)
+        self.fc1   = nn.Linear(32 * 16 * 16, 64)
+        self.fc2   = nn.Linear(64, 4)  # 4 classes
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))  # (B, 16, 32, 32)
@@ -22,37 +22,30 @@ class TrafficLightCNN(nn.Module):
         x = F.relu(self.fc1(x))
         return self.fc2(x)
 
-# Hyperparams
-batch_size = 32
-epochs = 10
-lr = 0.001
-
 # Transforms
-transform = transforms.Compose([
-    transforms.Resize((64, 64)),
-    transforms.ToTensor()
-])
+transform = transforms.Compose([transforms.Resize((64, 64)), transforms.ToTensor()])
 
 # Dataset
-dataset = datasets.ImageFolder('self_driving/data/color', transform=transform)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+dataset    = datasets.ImageFolder('self_driving/data/color', transform=transform)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 # Model, loss, optimizer
-model = TrafficLightCNN()
+model     = TrafficLightCNN()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=lr)
+optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
 # Training loop
-for epoch in range(epochs):
+for epoch in range(10):
     total_loss = 0
     for images, labels in dataloader:
         optimizer.zero_grad()
         outputs = model(images)
-        loss = criterion(outputs, labels)
+        loss    = criterion(outputs, labels)
+
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-    print(f"Epoch {epoch+1}/{epochs}, Loss: {total_loss:.4f}")
+    print(f"Epoch {epoch+1}/{10}, Loss: {total_loss:.4f}")
 
 # Save model
-torch.save(model.state_dict(), 'self_driving/simulator/models/color_model.pth')
+torch.save(model.state_dict(), 'self_driving/simulator/models/traffic_light_color_model.pth')
